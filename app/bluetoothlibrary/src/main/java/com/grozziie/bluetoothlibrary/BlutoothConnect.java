bf3f6ea6071177efb9f9989d35e8870307569079
Arifuljava <arifulislam.java.c@gmail.com>
2023-03-22 15:56:57 +0600

    Pa cksaggg

---
+++ app/bluetoothlibrary/src/main/java/com/grozziie/bluetoothlibrary/BlutoothConnect.java
@@ -0,0 +1,10 @@
+package com.grozziie.bluetoothlibrary;
+
+import android.content.Context;
+import android.widget.Toast;
+
+public class BlutoothConnect {
+    public  static  void mesgae(Context context,String message) {
+        Toast.makeText(context, ""+message, Toast.LENGTH_SHORT).show();
+    }
+}
